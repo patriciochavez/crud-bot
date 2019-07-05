eafb292cd2d5335d77acdee94e869aa4975160b6
patriciochavez <patricio_chavez@hotmail.com>
2019-07-05 14:59:17 -0300

    partial test

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,10 @@
 const express = require('express');
+const router = express.Router();
+const Task = require('../model/task');tasks
+
+router.get ('/', async (req, res) => {
+ const tasks = await Task.find();
+ res.render('index', {
+    //tasks
+ });
+});
